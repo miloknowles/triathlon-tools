2b8a79f240292712c4b06225e8a6c8b325910372
Milo Knowles <miloknowles97@gmail.com>
2024-03-10 10:32:18 -0400

    Paginate

--- app/src/app/results/[slug]/table.tsx
+++ app/src/app/results/[slug]/table.tsx
@@ -13,18 +13,30 @@ import {
 import { IronmanData } from "./types";
 import { ArrowDownIcon, ArrowLeftIcon, ArrowRightIcon, DownloadIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import AgeGroupMultiSelect from "./AgeGroupMultiSelect";
+import { useState } from "react";
 
 
 export default function ResultsTable({ data }: { data: IronmanData[] }) {
-  const page = 10;
-  const perPage = 10;
+  const [page, setPage] = useState(0);
+  const [ageGroups, setAgeGroups] = useState<string[]>(["overall"]);
 
-  const rows = data.slice(page*perPage, page*perPage + perPage).map((row) => {
+  const perPage = 10;
+  const maxPage = Math.ceil(data.length / perPage);
+
+  const filtered = data.filter((r: any) => {
+    if (ageGroups.includes("overall")) return true;
+    else if (ageGroups.includes("m-overall") && r.AgeGroup.startsWith("M")) return true;
+    else if (ageGroups.includes("f-overall") && r.AgeGroup.startsWith("F")) return true;
+    else return ageGroups.includes(r.AgeGroup);
+  });
+
+  const rows = filtered.slice(page*perPage, page*perPage + perPage).map((row) => {
     const status = <Badge size="xs">{row.EventStatus}</Badge>;
     const finishBadge = <Badge size="xs">{row.FinishRankGender}</Badge>
     const swimBadge = <Badge size="xs">{row.SwimRankGender}</Badge>;
     const bikeBadge = <Badge size="xs">{row.BikeRankGender}</Badge>
     const runBadge = <Badge size="xs">{row.RunRankGender}</Badge>;
+
     return (
       <TableRow key={row.Contact.FullName + row.AgeGroup}>
         <TableCell>{row.Contact.FullName || "?"} ({row.CountryISO2})</TableCell>
@@ -47,31 +59,39 @@ export default function ResultsTable({ data }: { data: IronmanData[] }) {
     );
   }).slice(0, 100);
 
+  const nextPage = () => {
+    setPage(Math.min(page + 1, maxPage));
+  }
+
+  const prevPage = () => {
+    setPage(Math.max(page - 1, 0));
+  }
+
   return (
     <Card className="mt-6">
       <div className="flex gap-3 flex-row">
         <MultiSelect className="" placeholder="Search Athlete(s)" icon={MagnifyingGlassIcon}>
           {names}
         </MultiSelect>
-        <AgeGroupMultiSelect className="max-w-[240px]"/>
-        <SearchSelect className="max-w-[240px]" placeholder="Rank By" defaultValue="gender" icon={ArrowDownIcon}>
+        <AgeGroupMultiSelect className="max-w-[240px]" selected={ageGroups} onChange={setAgeGroups}/>
+        <SearchSelect className="max-w-[240px]" placeholder="Rank Splits By" defaultValue="gender" icon={ArrowDownIcon}>
           <SearchSelectItem value="gender">Gender</SearchSelectItem>
           <SearchSelectItem value="overall">Overall</SearchSelectItem>
           <SearchSelectItem value="agegroup">Age Group</SearchSelectItem>
         </SearchSelect>
-        <Button icon={DownloadIcon}>Download</Button>
+        {/* <Button icon={DownloadIcon}>Download</Button> */}
       </div>
       <Table className="mt-3">
         <TableHead>
           <TableRow>
-          <TableHeaderCell>Name</TableHeaderCell>
-          <TableHeaderCell>Age Group</TableHeaderCell>
-          <TableHeaderCell>Finish</TableHeaderCell>
-          <TableHeaderCell>Swim</TableHeaderCell>
-          <TableHeaderCell>Bike</TableHeaderCell>
-          <TableHeaderCell>Run</TableHeaderCell>
-          <TableHeaderCell>T1</TableHeaderCell>
-          <TableHeaderCell>T2</TableHeaderCell>
+            <TableHeaderCell className="max-w-[200px]">Name</TableHeaderCell>
+            <TableHeaderCell>Age Group</TableHeaderCell>
+            <TableHeaderCell>Finish</TableHeaderCell>
+            <TableHeaderCell>Swim</TableHeaderCell>
+            <TableHeaderCell>Bike</TableHeaderCell>
+            <TableHeaderCell>Run</TableHeaderCell>
+            <TableHeaderCell>T1</TableHeaderCell>
+            <TableHeaderCell>T2</TableHeaderCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -79,8 +99,8 @@ export default function ResultsTable({ data }: { data: IronmanData[] }) {
         </TableBody>
       </Table>
       <div className="flex justify-end mt-3 gap-3">
-        <Button variant="secondary" icon={ArrowLeftIcon}>Previous</Button>
-        <Button icon={ArrowRightIcon} iconPosition="right">Next</Button>
+        <Button variant="secondary" icon={ArrowLeftIcon} onClick={prevPage} disabled={page === 0}>Previous</Button>
+        <Button icon={ArrowRightIcon} iconPosition="right" onClick={nextPage} disabled={page === maxPage}>Next</Button>
       </div>
     </Card>
   );
